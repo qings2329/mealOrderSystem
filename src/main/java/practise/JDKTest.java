df4790d3afba11585d4cd8e8647716abe2f79301
qings2329 <qings2329@sina.com>
2015-06-13 14:54:02 +0800

    JDKTest.java

---
+++ src/main/java/practise/JDKTest.java
@@ -0,0 +1,30 @@
+package practise;
+
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class JDKTest {
+	
+	
+	
+	public static void main(String args[]){
+		
+		
+		HashMap hmap = new HashMap(10);
+		
+		Hashtable ht = new Hashtable(10);
+		
+		ConcurrentHashMap cmap = new ConcurrentHashMap();
+		
+		
+		
+		
+		//
+		Integer x = new Integer(1);
+		
+		
+	}
+	
+	
+}
